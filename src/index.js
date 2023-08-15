93bd10590aba73cd40a6ff94f6a67348543fabe3
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-08-14 19:48:35 -0700

    running babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const multiply = (a,b) => a * b
+const firstNum = 2
+const secondNum = 3
+let product = multiply(firstNum, secondNum)
+console.log(`The product of ${firstNum} and ${secondNum} is ${product}`);
+
+const sum = (a,b) => a + b
+const firstAdd = 5
+const secondAdd = 15
+let total = sum(firstAdd, secondAdd)
+console.log(`The total of ${firstAdd} and ${secondAdd} is ${total}`);
